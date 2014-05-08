370a7b2e7158ba65e89c4b1241f4dcac46da4e06
Donovan S. Allen <dono.allen@gmail.com>
2014-05-08 10:31:58 -0600

    Plugin basic setup

---
+++ src/main/java/co/mcme/ai/mcme/AIMCME.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.mcme.ai.mcme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Donovan
+ */
+public class AIMCME extends JavaPlugin {
+    
+}
